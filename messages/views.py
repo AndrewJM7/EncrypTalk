08a9483c0f8bd537fe44281189f010c022eaf61e
AndrewJM7 <a.mason7@newcastle.ac.uk>
2024-04-30 14:38:49 +0100

    Chat page design

--- messages/views.py
+++ messages/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 from flask_socketio import send, join_room, leave_room, emit, disconnect
 from app import socketio
+from contacts.views import get_friends
 from models import User
 
 messages_blueprint = Blueprint('messages', __name__, template_folder='templates')
@@ -16,6 +17,7 @@ def messages():
 @messages_blueprint.route('/rooms')
 @login_required
 def rooms():
-    return render_template('messages/rooms.html')
+    friends = get_friends(current_user)
+    return render_template('messages/rooms.html', friends=friends)
 
 
