85e69df728642a4cabc53257d865536cbefaa54c
AndrewJM7 <a.mason7@newcastle.ac.uk>
2024-05-10 12:19:30 +0100

    Minor Register design adjustments

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import os
